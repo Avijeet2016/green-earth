6878e37214da3f814a991e59a15c636de5635b92
Avijeet <avijeet.cse1.bu@gmail.com>
2026-03-10 23:42:04 +0600

    all trees added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+const categoriesContainer = document.getElementById('categories-container');
+const treesContainer = document.getElementById('trees-container');
+
+
 const loadCategories = async () => {
     const url = "https://openapi.programming-hero.com/api/categories";
     const res = await fetch(url);
@@ -6,10 +10,7 @@ const loadCategories = async () => {
 }
 
 const displayCategories = (categories) => {
-    console.log(categories);
-    const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(item => {
-        console.log(item.category_name);
         const btn = document.createElement('button');
         btn.className = 'btn btn-outline w-full';
         btn.textContent = item.category_name;
@@ -17,4 +18,36 @@ const displayCategories = (categories) => {
     })
 }
 
+const loadTrees = async () => {
+    const url = "https://openapi.programming-hero.com/api/plants";
+    const res = await fetch(url);
+    const data = await res.json();
+    displayTrees(data.plants);
+}
+
+const displayTrees = (trees) => {
+    console.log(trees);
+    trees.forEach(item => {
+        const card = document.createElement('div');
+        card.className = "card bg-base-100 shadow-sm";
+        card.innerHTML = `
+            <figure>
+                <img src="${item.image}" alt="${item.name}" class="h-40 w-full" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">${item.name}</h2>
+                <p class="line-clamp-2">${item.description}</p>
+                <div class="badge badge-outline badge-success">${item.category}</div>
+                <div class="card-actions flex justify-between items-center">
+                    <h2 class="font-bold text-2xl text-green-500">$${item.price}</h2>
+                    <button class="btn btn-primary btn-sm">Buy Now</button>
+                </div>
+            </div>
+        `;
+        treesContainer.appendChild(card);
+    })
+    
+}
+
 loadCategories();
+loadTrees();
